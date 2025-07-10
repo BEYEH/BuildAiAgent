2d1cfc5194bbfeab8e36a18f53757d02704b5e43
Kells Ping <kells.ping@quanta.corp-partner.google.com>
2025-07-10 11:37:12 +0800

    [feat] update.

--- MyAiAgent/main.py
+++ MyAiAgent/main.py
@@ -1,27 +1,54 @@
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
+from pydantic import BaseModel
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.agents import create_tool_calling_agent, AgentExecutor
 
 load_dotenv()
 
 
+class ResearchResponse(BaseModel):
+    topic: str
+    tools_used: list[str]
+    summary: str
+    sources: list[str]
+
+
 def main():
     llm = ChatGroq(model="llama-3.3-70b-versatile")
+    tools = []
 
     query = input("User Input: ")
 
-    messages = [
-        (
-            "system",
-            """
-            You are a helpful assistant that translates English to Traditional Chinese. 
-            Translate the user sentence.
-            """,
-        ),
-        ("human", query),
-    ]
+    parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                """
+                You are a research assistant that will help generate a research paper.
+                Answer the user query and use neccessary tools. 
+                Wrap the output in this format and provide no other text\n{format_instructions}
+                """,
+            ),
+            ("placeholder", "{chat_history}"),
+            ("human", "{query}"),
+            ("placeholder", "{agent_scratchpad}"),
+        ]
+    ).partial(format_instructions=parser.get_format_instructions())
 
-    ai_msg = llm.invoke(messages)
-    print(ai_msg)
+    agent = create_tool_calling_agent(llm=llm, prompt=prompt, tools=tools)
+
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+    raw_response = agent_executor.invoke({"query": query})
+
+    try:
+        structured_response = parser.parse(raw_response.get("output"))
+        print("Result:\n", structured_response)
+    except Exception as e:
+        print("Exception: \n", e)
 
 
 if __name__ == "__main__":
